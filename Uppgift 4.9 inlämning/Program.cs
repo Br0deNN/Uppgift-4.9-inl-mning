ab7d044116e206366894c2e0ae36ef34825338d5
Br0deNN <theo.broden@gmail.com>
2023-10-20 21:16:01 +0200

    Add project files.

---
+++ Uppgift 4.9 inlämning/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+namespace inlämning_4._9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Detta programmet ritar ut en rätvinklig triangel");
+
+            Console.WriteLine("Skriv in hur många * du vill att kateten ska vara, skriv ett valfritt nummer");
+            int x = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < x; i++)
+            {
+                for (int j = 0; j < i; j++)
+                {
+                    Console.Write("*");
+                }
+                Console.WriteLine("*");
+            }
+            Console.ReadKey();
+        }
+    }
+}
